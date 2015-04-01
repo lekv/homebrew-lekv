920b9ba97e7522688abb63fa746e4ed50d48a8df
Lars Volker <lars.volker@sap.com>
2015-04-01 22:24:56 +0200

    FIX Add version to cppzmq

--- cppzmq.rb
+++ cppzmq.rb
@@ -3,6 +3,7 @@ require "formula"
 class Cppzmq < Formula
   homepage "http://zeromq.org/bindings:cpp"
   url "https://github.com/zeromq/cppzmq.git"
+  version "0.0.1-brew"
 
   def install
       system "mkdir -p #{include}"
